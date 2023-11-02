fbabe15c209d3a69b904e3a092d47a295bcf5282
AsetB <asetbakirov@gmail.com>
2023-11-02 16:14:47 +0600

    Initial Commit

---
+++ OzinsheDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OzinsheDemo
+//
+//  Created by Aset Bakirov on 02.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
